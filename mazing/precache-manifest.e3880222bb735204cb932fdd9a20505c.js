f9e6dce2ad535af7e7d6d8e31eca2f311671ac9d
Gert Hansen <gertqin@gmail.com>
2019-07-09 10:32:27 +0200

    Update mazing

---
+++ mazing/precache-manifest.e3880222bb735204cb932fdd9a20505c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mazing/robots.txt"
+  },
+  {
+    "revision": "c76b309f80773d467298",
+    "url": "/mazing/js/chunk-vendors.83393d2a.js"
+  },
+  {
+    "revision": "8cdff89ed4592d2abc19",
+    "url": "/mazing/js/app.49941489.js"
+  },
+  {
+    "revision": "ef405f389e457b24eab88f777edec1a8",
+    "url": "/mazing/index.html"
+  },
+  {
+    "revision": "8cdff89ed4592d2abc19",
+    "url": "/mazing/css/app.c15efd1f.css"
+  }
+];
